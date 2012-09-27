f7c7cdfb572d9e4a9b83a8a3d107922c35671175
Sigafoos <dan.j.conley@gmail.com>
2012-09-27 15:06:57 -0400

    Not much work, but restructuring the form a bit.

--- index.php
+++ index.php
@@ -5,24 +5,8 @@
 if ($_GET['action'] == "clean") {
 	echo "to do";
 } else if ($_GET['action'] == "fill") {
-	$query = "SELECT id, size FROM cbw_kegs WHERE status=2 ORDER BY size, id";
-	if (!($result = $db->query($query))) echo "<p>Something's gone wrong: #" . $db->errno . ": " . $db->error . "</p>";
-	while ($row = $result->fetch_assoc()) $kegs[] = new keg($row);
-?>
-	<p>Fill 'er up! Here are your choices:</p>
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<fieldset>
-	<?php
-	foreach ($kegs as $keg) {
-		$id = $keg->getid();
-		$intsize = $keg->getintsize();
-		$size = $keg->getsize();
-		echo "<label for=\"" . $id . "-" . $intsize . "\">" . $size . "  keg #" . $id . "</label><checkbox name=\"" . $id . "-" . $intsize . "\" id=\"keg[]\" /><br />\r";
-	}
-		?>
-			</fieldset>
-			</form>
-			<?php
+	if (!$GET['beer']) select_beer();
+	else select_kegs(2);
 } else if ($_GET['action'] == "carbonate") {
 	echo "to do";
 } else if ($_GET['action'] == "deliver") {
@@ -41,6 +25,40 @@ if ($_GET['action'] == "clean") {
 	<li><a href="?action=pickup">Pick Up</a></li>
 	</ul>
 <?php
+}
+function select_kegs($status) {
+	global $db;
+
+	$query = "SELECT id, size FROM cbw_kegs WHERE status=" . $status . " ORDER BY size, id";
+	if (!($result = $db->query($query))) echo "<p>Something's gone wrong: #" . $db->errno . ": " . $db->error . "</p>";
+	while ($row = $result->fetch_assoc()) $kegs[] = new keg($row);
+?>
+	<p>Fill 'er up! Here are your choices:</p>
+	<div id="formwrapper">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+	<fieldset>
+	<?php
+	$query = "SELECT id, size FROM cbw_keg_sizes";
+	if (!($result = $db->query($query))) echo "<p>Something's gone wrong: #" . $db->errno . ": " . $db->error . "</p>";
+	while ($row = $result->fetch_assoc()) $sizes[$row['id']] = $row['size'];
+
+	foreach ($kegs as $keg) {
+		$id = $keg->getid();
+		$intsize = $keg->getsize();
+		$size = $sizes[$intsize];
+		//echo "<input type=\"radio\" id=\"" . $id . "_
+		echo "<checkbox name=\"" . $id . "_" . $intsize . "\" id=\"keg[]\" /><label for=\"" . $id . "_" . $intsize . "\">" . $size . "  keg #" . $id . "</label><br />\r";
+	}
+		?>
+			</fieldset>
+			</form>
+			</div>
+			<?php
+}
+
+// I don't see this being used more than once, but if it is...
+function select_beer() {
+	echo "WHICH BEER?";
 }
 require('footer.inc.php');
 ?>
