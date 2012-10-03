b936f45bab3e4568e357880253b2ef6692b364fe
Sigafoos <dan.j.conley@gmail.com>
2012-10-03 10:44:59 -0400

    JQTouch is now working. Huzzah.

--- header.inc.php
+++ header.inc.php
@@ -3,6 +3,13 @@
 <html lang="en">
 <head>
 <title>Kegs!</title>
-<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<!--<link href="style.css" rel="stylesheet" type="text/css" media="screen" />-->
+<!-- JQTouch! -->
+<style type="text/css" media="screen">@import "../jqtouch/themes/css/jqtouch.css";</style>
+<script src="../jqtouch/src/lib/zepto.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="../jqtouch/src/jqtouch.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+$.jQTouch({ });
+</script>
 </head>
 <body>
