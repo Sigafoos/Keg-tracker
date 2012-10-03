7df647b9ae08c00fed93678287fe48d68c242549
Sigafoos <dan.j.conley@gmail.com>
2012-10-03 13:39:55 -0400

    The start of JQTouch stuff. I may be heading down the wrong path, but it works, sorta, so far.

--- header.inc.php
+++ header.inc.php
@@ -5,11 +5,23 @@
 <title>Kegs!</title>
 <!--<link href="style.css" rel="stylesheet" type="text/css" media="screen" />-->
 <!-- JQTouch! -->
-<style type="text/css" media="screen">@import "../jqtouch/themes/css/jqtouch.css";</style>
-<script src="../jqtouch/src/lib/zepto.min.js" type="text/javascript" charset="utf-8"></script>
-<script src="../jqtouch/src/jqtouch.min.js" type="text/javascript" charset="utf-8"></script>
+<style type="text/css" media="screen">@import "jqtouch/themes/css/apple.css";</style>
+<script src="jqtouch/src/lib/zepto.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="jqtouch/src/jqtouch.min.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
 $.jQTouch({ });
+
+$(document).ready(function(){
+		// capture all taps and load them from a named php file
+		// this is because as far as I can tell,
+		// onclick() isn't recognized in iOS and ontap() in desktop
+		$('#home li a').on("click",function() {
+			// there will also be the class "active" that we need to strip out
+			var section = this.className.split(/\s/)[0];
+			$('#'+section+' .content').load(section+'.php');
+			});
+		});
+</script>
 </script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,64 +1,25 @@
 <?php require('header.inc.php'); ?>
-<h1>Keg statuses</h1>
 
-<?php 
-if ($_GET['action'] == "clean") {
-	echo "to do";
-} else if ($_GET['action'] == "fill") {
-	if (!$GET['beer']) select_beer();
-	else select_kegs(2);
-} else if ($_GET['action'] == "carbonate") {
-	echo "to do";
-} else if ($_GET['action'] == "deliver") {
-	echo "to do";
-} else if ($_GET['action'] == "pickup") {
-	echo "to do";
-} else {
-?>
-	<p>What'll it be, mister?</p>
+<div id="home">
+<div class="toolbar">
+<h1>Kegs!</h1>
+</div>
 
-	<ul>
-	<li><a href="?action=clean">Clean</a></li>
-	<li><a href="?action=fill">Fill</a></li>
-	<li><a href="?action=carbonate">Carbonate</a></li>
-	<li><a href="?action=deliver">Deliver</a></li>
-	<li><a href="?action=pickup">Pick Up</a></li>
-	</ul>
-<?php
-}
-function select_kegs($status) {
-	global $db;
+<div class="info"><?php echo date("h:m:s"); ?></div>
 
-	$query = "SELECT id, size FROM cbw_kegs WHERE status=" . $status . " ORDER BY size, id";
-	if (!($result = $db->query($query))) echo "<p>Something's gone wrong: #" . $db->errno . ": " . $db->error . "</p>";
-	while ($row = $result->fetch_assoc()) $kegs[] = new keg($row);
-?>
-	<p>Fill 'er up! Here are your choices:</p>
-	<div id="formwrapper">
-	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<fieldset>
-	<?php
-	$query = "SELECT id, size FROM cbw_keg_sizes";
-	if (!($result = $db->query($query))) echo "<p>Something's gone wrong: #" . $db->errno . ": " . $db->error . "</p>";
-	while ($row = $result->fetch_assoc()) $sizes[$row['id']] = $row['size'];
+<ul class="rounded">
+<li><a href="#clean" class="clean">Clean</a></li>
+<li><a href="#fill">Fill</a></li>
+</ul>
+</div>
 
-	foreach ($kegs as $keg) {
-		$id = $keg->getid();
-		$intsize = $keg->getsize();
-		$size = $sizes[$intsize];
-		//echo "<input type=\"radio\" id=\"" . $id . "_
-		echo "<checkbox name=\"" . $id . "_" . $intsize . "\" id=\"keg[]\" /><label for=\"" . $id . "_" . $intsize . "\">" . $size . "  keg #" . $id . "</label><br />\r";
-	}
-		?>
-			</fieldset>
-			</form>
-			</div>
-			<?php
-}
+<div id="clean">
+<div class="toolbar">
+<a class="back" href="#home">Back</a>
+<h1>Clean kegs</h1>
+</div>
+<div class="content"></div>
 
-// I don't see this being used more than once, but if it is...
-function select_beer() {
-	echo "WHICH BEER?";
-}
-require('footer.inc.php');
-?>
+</div>
+
+<?php require('footer.inc.php'); ?>
