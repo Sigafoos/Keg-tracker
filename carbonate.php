bbe3427e9098723e3d63311c343b52ca11fc5aee
Sigafoos <dan.j.conley@gmail.com>
2012-10-04 19:59:13 -0400

    Starting on carbonating kegs

---
+++ carbonate.php
@@ -0,0 +1,10 @@
+<?php 
+require('functions.php');
+?>
+<div id="carbonate">
+<div class="toolbar">
+<a class="back" href="#home">Back</a>
+<h1>Choose kegs</h1>
+</div>
+<?php select_kegs(3); ?>
+</div>
--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 <ul class="rounded">
 <li><a href="#clean">Clean</a></li>
 <li><a href="?section=fill#beer">Fill</a></li>
+<li><a href="?section=fill#carbonate">Carbonate</a></li>
+<li><a href="?section=deliver#beer">Deliver</a></li>
 </ul>
 </div>
 
 <div id="beer"></div>
 <div id="clean"></div>
 <div id="fill"></div>
+<div id="carbonate"></div>
+<div id="deliver"></div>
 <div id="newbeer">hey</div>
 <div id="test"></div>
 
