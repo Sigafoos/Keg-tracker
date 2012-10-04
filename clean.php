100c23c227bc52eeebb717d80fcbb634831c813b
Sigafoos <dan.j.conley@gmail.com>
2012-10-04 19:21:02 -0400

    Was trying to clean already clean kegs

--- clean.php
+++ clean.php
@@ -6,6 +6,5 @@ require('functions.php');
 <a class="back" href="#home">Back</a>
 <h1>Choose kegs</h1>
 </div>
-<div class="info"><?php echo $_GET['beer']; ?></div>
-<?php select_kegs(2); ?>
+<?php select_kegs(1); ?>
 </div>
