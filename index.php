a507c67d72e65848ad534a9455a125261d9e770c
Sigafoos <dan.j.conley@gmail.com>
2012-10-04 20:14:34 -0400

    Adding the framework for reports, though there aren't any yet

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
 <h1>Kegs!</h1>
 </div>
 
+<h2>Actions</h2>
 <ul class="rounded">
 <li><a href="#clean">Clean</a></li>
 <li><a href="?section=fill#beer">Fill</a></li>
 <li><a href="?section=fill#carbonate">Carbonate</a></li>
 <li><a href="?section=deliver#location">Deliver</a></li>
+</ul>
+
+<h2>Reports</h2>
+<ul class="rounded">
+<li><a href="#">Stuff?</a></li>
 </ul>
 </div>
 
@@ -17,6 +23,7 @@
 <div id="fill"></div>
 <div id="carbonate"></div>
 <div id="deliver"></div>
+<div id="return"></div>
 
 <div id="beer"></div>
 <div id="location"></div>
