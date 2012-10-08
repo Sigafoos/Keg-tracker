feb32b664fd164f6d3c7df387367511ce321686b
Sigafoos <dan.j.conley@gmail.com>
2012-10-08 11:24:29 -0400

    I've been meaning to ditch the Apple css

--- header.inc.php
+++ header.inc.php
@@ -4,7 +4,7 @@
 <title>Kegs!</title>
 <!--<link href="style.css" rel="stylesheet" type="text/css" media="screen" />-->
 <!-- JQTouch! -->
-<style type="text/css" media="screen">@import "jqtouch/themes/css/apple.css";</style>
+<style type="text/css" media="screen">@import "jqtouch/themes/css/jqtouch.css";</style>
 <script src="jqtouch/src/lib/zepto.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="jqtouch/src/jqtouch.min.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
