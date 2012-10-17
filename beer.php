895f8d73bb9428a2894024f10a5a71ae5a330837
Sigafoos <dan.j.conley@gmail.com>
2012-10-16 22:00:18 -0400

    Whoops, beer.php was broken

--- beer.php
+++ beer.php
@@ -1,12 +1,12 @@
 <?php 
+require('header.inc.php');
 require('functions.php');
 ?>
-<div id="beer">
-<div class="toolbar">
-<a class="back" href="#home">Back</a>
-<a class="button slideup" href="#newbeer">+</a>
+<div data-role="header">
 <h1>Choose beer</h1>
 </div>
+
+<div data-role="content">
 <?php select_beer($_GET['section']); ?>
 </div>
 <?php require('footer.inc.php'); ?>
