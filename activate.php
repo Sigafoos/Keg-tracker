495b14c6ec57af24cd88346cc37f7af5908f928f
Sigafoos <dan.j.conley@gmail.com>
2012-11-30 12:37:19 -0500

    Switching from buttoms to a list. Need to style.

--- activate.php
+++ activate.php
@@ -12,20 +12,24 @@ require('header.inc.php');
 <div data-role="collapsible-set" data-collapsed-icon="arrow-d" data-expanded-icon="arrow-u">
 <div data-role="collapsible">
 <h2>Beers</h2>
+<ul data-role="listview" data-inset="true">
 <?php
 $query = "SELECT id, beer FROM cbw_beers WHERE active=0 AND id>0 ORDER BY beer";
 if (!($result = $db->query($query))) echo "<p>Oh my: #" . $db->errno . ": " . $db->error . "</p>\r";
-while ($row = $result->fetch_assoc()) echo "<p><a href=\"#\" id=\"b" . $row['id'] . "\" data-role=\"button\" data-icon=\"plus\">" . $row['beer'] . "</a></p>\r\n";
+while ($row = $result->fetch_assoc()) echo "<li><a href=\"#\" id=\"b" . $row['id'] . "\" data-icon=\"plus\">" . $row['beer'] . "</a></li>\r\n";
 ?>
+</ul>
 </div>
 
 <div data-role="collapsible">
 <h2>Locations</h2>
+<ul data-role="listview" data-inset="true">
 <?php
 $query = "SELECT id, location FROM cbw_locations WHERE active=0 AND id>0 ORDER BY location";
 if (!($result = $db->query($query))) echo "<p>Oh my: #" . $db->errno . ": " . $db->error . "</p>\r";
-while ($row = $result->fetch_assoc()) echo "<p><a href=\"#\" id=\"l" . $row['id'] . "\" data-role=\"button\" data-icon=\"plus\">" . $row['location'] . "</a></p>\r\n";
+while ($row = $result->fetch_assoc()) echo "<li><a href=\"#\" id=\"l" . $row['id'] . "\" data-icon=\"plus\">" . $row['location'] . "</a></li>\r\n";
 ?>
+</ul>
 </div>
 </div>
 </div>
