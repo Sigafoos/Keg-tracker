fc2653bb0a62e0fa7e80ab946365130636d5925c
Sigafoos <dan.j.conley@gmail.com>
2012-12-22 17:52:06 -0500

    Working on adding a new keg

---
+++ newkeg.php
@@ -0,0 +1,46 @@
+<?php 
+require('functions.php');
+
+if ($_POST['update']) {
+	$query = "SELECT max(id) AS id FROM " . $dbprefix . "kegs WHERE size=" . $_POST['size'];
+	if (!($result = $db->query($query))) echo "<p>Oh my: #" . $db->errno . ": " . $db->error . "</p>\r";
+	$row = $result->fetch_assoc();
+	echo "<input type=\"number\" pattern=\"[0-9]*\" name=\"id\" id=\"id\" value=\"" . ($row['id']+1) . "\" class=\"ui-input-text ui-body-c ui-corner-all ui-shadow-inset\" />";
+	die();
+}
+
+require('header.inc.php');
+?>
+<div data-role="header">
+<a href="#" data-rel="back" data-type="button" data-theme="b" data-icon="arrow-l">Back</a>
+<h1>New keg</h1>
+</div>
+
+<div data-role="content">
+<form action="" method="post">
+<div data-role="fieldcontain">
+<label for="size">Size</label>
+<select name="size" id="size">
+<?php
+$query = "SELECT id, size FROM " . $dbprefix . "keg_sizes ORDER BY id";
+if (!($result = $db->query($query))) echo "<p>Oh my: #" . $db->errno . ": " . $db->error . "</p>\r";
+while ($row = $result->fetch_assoc()) echo "<option value=\"" . $row['id'] . "\">" . $row['size'] . "</option>\r\n";
+?>
+</select>
+</div>
+
+<div data-role="fieldcontain">
+<label for="id">First id</label>
+<input type="number" pattern="[0-9]*" name="id" id="id" />
+</div>
+
+<div data-role="fieldcontain">
+<label for="end">Second id</label>
+<input type="number" pattern="[0-9]*" name="end" id="end" placeholder="Leave blank if only adding one keg" />
+</div>
+
+</form>
+</div>
+
+<?php require('footer.inc.php'); ?>
+
