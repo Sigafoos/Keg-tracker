25f634c9044232b24ea802cbc54cbcb27f9fc0b4
Sigafoos <dan.j.conley@gmail.com>
2013-04-09 20:59:38 -0400

    My iPhone isn't playing nicely with the minified js. Screw it, I don't have time for this, let's un-minify for now.

--- header.inc.php
+++ header.inc.php
@@ -6,6 +6,6 @@
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
-<script src="custom-min.js"></script>
+<script src="custom.js"></script>
 <body>
 <div data-role="page">
