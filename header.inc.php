ea27301c71ff78807d499ff9ca7f4c7a2f5f57a8
Sigafoos <dan.j.conley@gmail.com>
2013-08-22 06:41:39 -0400

    Added an image when adding to the iOS home screen

--- header.inc.php
+++ header.inc.php
@@ -3,6 +3,8 @@
 <head>
 <title>Kegs!</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="apple-touch-icon" href="keg.png"/>
+<meta name="apple-mobile-web-app-capable" content="yes" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
