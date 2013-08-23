6f771cea8579d6a2f9db9fca44b9743510d8e574
Sigafoos <dan.j.conley@gmail.com>
2013-08-22 20:25:45 -0400

    Upgrading jQuery mobile to 1.3.2

--- header.inc.php
+++ header.inc.php
@@ -5,9 +5,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="apple-touch-icon" href="keg.png"/>
 <meta name="apple-mobile-web-app-capable" content="yes" />
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
-<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 <script src="custom.js"></script>
 <body>
 <div data-role="page">
