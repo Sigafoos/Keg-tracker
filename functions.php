7455345da66e85276e8706f4673ad06f20598211
Sigafoos <dan.j.conley@gmail.com>
2013-10-04 16:13:55 -0400

    Warnings log differently

--- functions.php
+++ functions.php
@@ -135,7 +135,13 @@ class keg {
 		global $db,$dbprefix;
 		if (!$timestamp) $timestamp = date("Y-m-d G:i:s");
 
-		$query = "INSERT INTO " . $dbprefix . "keg_log(keg_id, size, status, location, beer, warning, date) VALUES(" . $this->id . "," . $this->size . ", " . $this->status . ", " . $this->location . ", " . $this->beer . ", " . $this->warning . ", '" . $timestamp . "')";
+		$query = "INSERT INTO " . $dbprefix . "keg_log(keg_id, size, status";
+		if ($this->status != -99) $query .= ", location, beer";
+		else $query .= ", warning";
+		$query .= ", date) VALUES(" . $this->id . "," . $this->size . ", " . $this->status;
+		if ($this->status != -99) $query .= ", " . $this->location . ", " . $this->beer;
+		else $query .= ", " . $this->warning;
+		$query .= ", '" . $timestamp . "')";
 		if (!$db->query($query)) {
 			echo "<p>Error logging update for keg " . $this->id . "-" . $this->size . ": #" . $db->errno . ": " . $db->error . "</p>\r";
 			echo "<p>" . $query . "</p>\r";
