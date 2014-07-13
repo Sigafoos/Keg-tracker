5aff4aa5253ce24d72a5509640509221c125e534
Sigafoos <dan.j.conley@gmail.com>
2014-07-13 17:07:32 -0400

    Installing appears to work

--- header.inc.php
+++ header.inc.php
@@ -9,3 +9,19 @@
 <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 <body>
 <div data-role="page">
+<?php 
+if (file_exists("install.php")) {
+	?>
+		<div data-role="header">
+		<h1>Error</h1>
+		</div>
+		<div data-role="content">
+		Install file exists. If this is your first time on the site, fill out config.inc.php and then install. If you've already installed, remove install.php.
+
+		<p><a href="install.php" data-role="button" data-theme="b">Install</a></p>
+		</div>
+		<?php
+	require('footer.inc.php');
+	die();
+}
+?>
